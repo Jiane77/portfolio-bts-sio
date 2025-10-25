6e7b788c01a48ed94759aadcbc05c4f9cb2eb94b
Jiane <haroundjihane0@gmail.com>
2025-10-26 00:44:26 +0200

    Remove unused React import

--- src/assets/components/techWatch.tsx
+++ src/assets/components/techWatch.tsx
@@ -1,5 +1,3 @@
-
-import React from 'react'
 import { motion } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 import {Rss, ExternalLink, Calendar, TrendingUp, BookOpen, Globe} from 'lucide-react'
