e0c9b7b9af18f8f04e92bf0f944a352b0c652f70
Jiane <haroundjihane0@gmail.com>
2025-10-27 18:14:18 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import type { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   let build: UserConfig['build'], esbuild: UserConfig['esbuild'], define: UserConfig['define']
 
