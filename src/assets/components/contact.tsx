249263ff14002a26da64b9053f3ae59b5f2fd57e
Jiane <haroundjihane0@gmail.com>
2025-11-02 23:41:11 +0100

    add docker link

--- src/assets/components/contact.tsx
+++ src/assets/components/contact.tsx
@@ -1,5 +1,4 @@
-import { Mail, Phone, Linkedin, Github } from "lucide-react"
-
+import { Mail, Phone, Linkedin, Github} from "lucide-react";
 const Contact = () => {
   return (
     <section
@@ -14,7 +13,8 @@ const Contact = () => {
         <div className="space-y-5 text-lg">
           <p className="text-start font-medium">🌸 Nom : <span className="text-purple-300">Haroun</span></p>
           <p className="text-start font-medium">🌸 Prénom : <span className="text-purple-300">Djihane</span></p>
-          <div className=" text-start flex items-center justify-start gap-3">
+
+          <div className="text-start flex items-center justify-start gap-3">
             <Mail className="text-purple-400" />
             <a href="mailto:haroundjihane0@gmail.com" className="hover:text-purple-300 underline">
               haroundjihane0@gmail.com
@@ -27,14 +27,25 @@ const Contact = () => {
               +33 6 68 45 08 56
             </a>
           </div>
-
+          <div className="flex items-center justify-start gap-3">
+          <span className="text-purple-400 text-2xl">🐳</span>
+            <a
+              href="https://hub.docker.com/u/jiane77"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-purple-300 underline"
+            >
+              Mon Docker
+            </a>
+          </div>
           <div className="flex items-center justify-start gap-3">
             <Linkedin className="text-purple-400" />
             <a
               href="https://www.linkedin.com/in/djihane-haroun-183068348/"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-purple-300 underline">
+              className="hover:text-purple-300 underline"
+            >
               Djihane Haroun
             </a>
           </div>
@@ -42,17 +53,18 @@ const Contact = () => {
           <div className="flex items-center justify-start gap-3">
             <Github className="text-purple-400" />
             <a
-              href="https://github.com/Jiane77" 
+              href="https://github.com/Jiane77"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-purple-300 underline">
+              className="hover:text-purple-300 underline"
+            >
               Mon GitHub
             </a>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
